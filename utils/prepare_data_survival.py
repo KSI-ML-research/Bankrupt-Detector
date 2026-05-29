70df21d03421b868a3d7ef1dd2bd39fc2878c8f5
BartekBv <bartekbudzinski1b@gmail.com>
2026-05-29 21:19:52 +0200

    fix: bugfix in prepare_data_survival.py

--- utils/prepare_data_survival.py
+++ utils/prepare_data_survival.py
@@ -1,7 +1,7 @@
 import pandas as pd
 import numpy as np
 
-data = pd.read_csv('../data/dataset_invoices.csv')
+data = pd.read_csv('data/dataset_invoices.csv')
 
 for col in ['document_create_date', 'clear_date', 'due_in_date']:
     data[col] = pd.to_datetime(data[col], dayfirst=True, errors='coerce')
@@ -22,4 +22,4 @@ treshold = client_counts.quantile(0.90)
 key_enterprises = client_counts[client_counts > treshold].index
 data['segment'] = np.where(data['cust_number'].isin(key_enterprises), 'BE', 'SME')
 
-data.to_csv('../data/dataset_survival.csv', index=False)
+data.to_csv('data/dataset_survival.csv', index=False)
